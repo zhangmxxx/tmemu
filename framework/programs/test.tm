28a0b80dc660b151f83bff4ebd1d34b9c40ab124
Simenons <211240005@smail.nju.edu.cn>
2023-12-13 15:39:37 +0800

    (debug version) finish parse tmfile and check input

--- framework/programs/test.tm
+++
@@ -1,2 +0,0 @@
-; the finite set of states
-#Q = {0,1,2,3,a_Z0092_lxt}
--- framework/turing-project/main.cpp
+++ framework/turing-project/main.cpp
@@ -2,27 +2,50 @@
 #include <fstream>
 #include <string>
 #include <set>
+#include <vector>
+#include <cassert>
 using namespace std;
 
 // 其实只需要在运行时考虑verbose. 其余的一律cerr.
 /* const */
 const string help_msg = "usage: turing [-v|--verbose] [-h|--help] <tm> <input>";
-const string parse_err_msg = "parse tmfile error! check the format. Good Luck!";
+const string parse_err_msg = "syntax error";
 const string input_err_msg = "illegal input string";
+
+/* macro definition */
+#define STRINGIFY(s)  #s
+#define TOSTRING(s) STRINGIFY(s)
+#define Assert(cond) \
+({ if (!(cond)) { \
+  cerr << parse_err_msg << endl; \
+  cerr << " @ " << __FILE__ << ":" << TOSTRING(__LINE__) << "  \n"; \
+  exit(1); \
+}})
+
 
 bool verbose = false;
 
-void Split(const string& s, vector<string>& v, const string& c);
+struct transition {
+  string _state, state_; // state
+  string _sym, sym_; // tape symbol combinition
+  string dir; // moving direction
+};
+
+void Split(const string &src, vector<string> &ret, const string& c);
+void extract(const string &src, vector<string> &ret);
 int parse_tmfile(string &filename);
 bool check_input(string &input);
+int strtoint(string &input);
 
 /* turing machine definitions */
-vector<string> Q;
-set<char> S, G;
-int q0; // index
-char B;
-set<int> F; // index
-int N;
+static set<string> Q;
+static set<char> S, G;
+static string q0;
+static char B;
+static set<string> F;
+static int N;
+static int tapenum;
+static vector<transition> delta;
 
 int main(int argc, char* argv[]){
   string file = "", input = "";
@@ -48,12 +71,44 @@ int main(int argc, char* argv[]){
     cerr << "Unable to open file: " << file << endl;
     return 1;
   }
-  else if (ret == 1) {
-    cerr << parse_err_msg << endl;
-    return 1;
+
+  cerr << "Q:\n";
+  for (auto state : Q) {
+    cerr << state << " ";
   }
-  else {;}
-  /* check input */
+  cerr << endl;
+
+  cerr << "S:\n";
+  for (auto state : S) {
+    cerr << state << " ";
+  }
+  cerr << endl;
+
+  cerr << "G:\n";
+  for (auto state : G) {
+    cerr << state << " ";
+  }
+  cerr << endl;
+
+  cerr << "S:\n";
+  cerr << q0 << endl;
+
+  cerr << "B:\n";
+  cerr << B << endl;
+
+  cerr << "F:\n";
+  for (auto state : F) {
+    cerr << state << " ";
+  }
+  cerr << endl;
+
+  cerr << "N:\n";
+  cerr << tapenum << endl;
+
+  for (auto t : delta) {
+    cerr << t._state << " " << t._sym << " " << t.sym_ << " " << t.dir << " " << t.state_ << endl;
+  }
+
   if (!check_input(input)) {
     cerr << input_err_msg << endl;
     return 1;
@@ -62,74 +117,86 @@ int main(int argc, char* argv[]){
 
 int parse_tmfile(string &filename) {
   ifstream infile(filename);
-  if (!infile.is_open()) {
-    cerr << "Unable to open file: " << filename << endl;
-    return -1;
-  }
+  if (!infile.is_open()) return -1;
   string line;
   /* for each line in file */
   while (getline(infile, line)) {
+    vector<string> part; part.clear();
+    /* cut off comment */
+    string::size_type end = 0;
+    while (end < line.length() && line[end] != ';') end++;
+    line = line.substr(0, end);
     /* empty line */
     if (line.length() == 0) { continue; }
-    /* comment */
-    else if (line[0] == ';') { cerr << "comment: " << line << endl; }
-    /* turing machine define */
-    else if (line[0] == '#') {
-      char type = line[1];
-      string::size_type pos = 0;
-      while (line[pos] != '=') { pos++; } // points to '='
+
+    Split(line, part, " ");
+    /* tm definition */
+    if (line[0] == '#') {
       vector<string> elements; elements.clear();
-      if (type == 'Q') {
-        pos += 3;
-        Split(line.substr(pos, line.size() - pos - 1), elements, ",");
-        for (int i = 0; i < elements.size(); ++i) {
-          cerr << elements[i] << endl;
-        }
-      }
-      else if (type == 'S') {
-
-      }
-      else if (type == 'G') {
-
-      }
-      else if (type == 'q') {
-
-      }
-      else if (type == 'B') {
-
-      }
-      else if (type == 'F') {
-
-      }
-      else if (type == 'N') {
-
-      }
-      else {
-        cerr << "invalid #type " << type << endl;
-        return 1;
+      extract(part[2], elements);
+      for (auto elem : elements) {
+        bool is_char = elem.length() == 1;
+        if (part[0] == "#Q") { Q.insert(elem); }
+        else if (part[0] == "#S") { Assert(is_char); S.insert(elem[0]); }
+        else if (part[0] == "#G") { Assert(is_char); G.insert(elem[0]); }
+        else if (part[0] == "#q0") { q0 = elem; }
+        else if (part[0] == "#B") { Assert(is_char); B = elem[0]; }
+        else if (part[0] == "#F") { F.insert(elem); }
+        else if (part[0] == "#N") { tapenum = strtoint(elem); }
+        else { Assert(0); }
       }
     }
-    /* transition functions */
     else {
-
+      transition t;
+      Assert(part.size() == 5);
+      t._state = part[0]; t.state_ = part[4];
+      t._sym = part[1]; t.sym_ = part[2];
+      t.dir = part[3];
+      Assert(t._sym.length() == tapenum && t.sym_.length() == tapenum && t.dir.length() == tapenum);
+      delta.push_back(t);
     }
   }
+
+  /* do some check for the turing machine */
+  // S \subseteq G
+  Assert(B == '_');
+  Assert(G.find('*') == G.end());
+  Assert(S.find('*') == S.end());
+
   infile.close();
   return 0;
 }
 
 bool check_input(string &input) {
+  for (int i = 0; i < input.size(); ++i) {
+    if (S.find(input[i]) == S.end()) return false;
+  }
   return true;
 }
 
-void Split(const string& s, vector<string>& v, const string& c){
+void Split(const string &src, vector<string> &ret, const string& c){
   string::size_type pos1, pos2;
-  pos2 = s.find(c);
+  pos2 = src.find(c);
   pos1 = 0;
   while (string::npos != pos2) {
-    v.push_back(s.substr(pos1, pos2 - pos1));
+    ret.push_back(src.substr(pos1, pos2 - pos1));
     pos1 = pos2 + c.size();
-    pos2 = s.find(c, pos1);
+    pos2 = src.find(c, pos1);
   }
-  if (pos1 != s.length()) v.push_back(s.substr(pos1));
+  if (pos1 != src.length()) ret.push_back(src.substr(pos1));
+}
+
+void extract(const string &src, vector<string> &ret) {
+  string ct = src;
+  if (ct[0] == '{') ct = ct.substr(1, ct.length() - 2);
+  Split(ct, ret, ",");
+}
+
+int strtoint(string &input) {
+  int ans = 0;
+  for (int i = 0; i < input.length(); ++i) {
+    ans *= 10;
+    ans += input[i] - '0';
+  }
+  return ans;
 }
